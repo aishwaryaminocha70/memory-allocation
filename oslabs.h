496908b3f62f9e6c6712cc2f144877c90d97caa8
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-09 19:16:01 +0530

    Update oslabs.h

--- oslabs.h
+++ oslabs.h
@@ -58,3 +58,4 @@ struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *qu
 struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp);
 struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum);
 struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp, int time_quantum);
+
